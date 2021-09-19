b9f031bb4a3f6ce500f67f5e722b727d52d65dba
Tellur89 <62219709+Tellur89@users.noreply.github.com>
2021-09-19 20:28:46 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+function checkValue() {
+  var ing1 = document.getElementById("input1").value;
+  var ing2 = document.getElementById("input2").value;
+  var ing3 = document.getElementById("input3").value;
+  var ing4 = document.getElementById("input4").value;
+
+  if (
+    (ing1 == "gowno" || ing1 == "dupa" || ing1 == "fiut" || ing1 == "maslo") &&
+    (ing2 == "gowno" || ing2 == "dupa" || ing2 == "fiut" || ing2 == "maslo") &&
+    (ing3 == "gowno" || ing3 == "dupa" || ing3 == "fiut" || ing3 == "maslo") &&
+    (ing4 == "gowno" || ing4 == "dupa" || ing4 == "fiut" || ing4 == "maslo")
+  ) {
+    document.getElementById("form").setAttribute("action", "/wskazowka.html");
+  } else {
+    console.log("Error");
+  }
+}
